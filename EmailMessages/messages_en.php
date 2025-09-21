b0301cc3d478ffe2bc8022159b0a962be0c24431
khillon-makwana <khillon2005@gmail.com>
2025-09-21 17:39:51 +0300

    Added messages_en.php

---
+++ EmailMessages/messages_en.php
@@ -0,0 +1,14 @@
+<?php
+$msg['verify_subject'] = "Account Activation Code for {{site_name}}";
+
+$msg['verify_body'] = "
+Hello {{fullname}},<br><br>
+
+You signed up for an account on <strong>{{site_name}}</strong>.<br>
+Your activation code is:
+<h1>{{activation_code}}</h1>
+Please enter it on the site when prompted to.<br><br>
+Regards,<br>
+Systems Admin,<br>
+{{site_name}}
+";
