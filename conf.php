246d6c826e4a7435e40b1c41cd35135608753f37
Suraj-231 <suraj.menariya@strathmore.edu>
2025-09-21 22:24:53 +0300

    added conf.php

---
+++ conf.php
@@ -0,0 +1,28 @@
+<?php
+// Site Information
+$conf['site_name'] = 'EventHub';
+$conf['site_url'] = 'http://localhost/EVENTHUB';
+$conf['admin_email'] = 'admin@eventhub.com';
+
+// Database Configuration
+$conf['db_type'] = 'pdo';
+$conf['db_host'] = 'localhost';
+$conf['db_user'] = 'root';
+$conf['db_pass'] = '';
+$conf['db_name'] = 'eventhub';
+$conf['db_port'] = 3307; 
+
+// Site Language
+$conf['site_lang'] = 'en';
+
+// Email Configurations
+$conf['mail_type'] = 'smtp'; // Options: 'smtp' or 'mail'
+$conf['smtp_host'] = 'smtp.gmail.com';
+$conf['smtp_user'] = 'suraj.menariya@strathmore.edu';
+$conf['smtp_pass'] = '';
+$conf['smtp_port'] = 465;
+$conf['smtp_secure'] = 'ssl';
+
+$conf['min_password_length'] = 8;
+
+$conf['valid_email_domains'] = ['eventhub.com', 'gmail.com', 'yahoo.com', 'outlook.com', 'strathmore.edu'];
